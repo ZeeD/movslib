0296cba550073ca612d172e26f9d7bcfb704ae39
Vito De Tullio <vito.detullio@gmail.com>
2022-10-25 14:11:12 +0200

    update project layout

--- setup.py
+++ setup.py
@@ -2,17 +2,11 @@ from setuptools import find_packages
 from setuptools import setup
 
 setup(name='movs',
-      version='0.0.0',
-      url='http://www.example.com',
+      version='0.0.1',
+      url='https://github.com/ZeeD/movs',
       author='Vito De Tullio',
       author_email='vito.detullio@gmail.com',
       py_modules=find_packages(),
-      extra_require={
-          'dev': [
-              'distutil'
-          ]
-      },
-      package_dir={'': 'src'},
       package_data={
           'movs': ['py.typed'],
       })
