fb3b64674e21782bb0070e5dc09a4eaa7d3052b4
Vito De Tullio <vito.detullio@gmail.com>
2023-09-09 13:59:36 +0200

    add support for scansioni

---
+++ movs/scansioni/__init__.py
@@ -0,0 +1,48 @@
+from datetime import date
+from datetime import datetime
+from decimal import Decimal
+from typing import overload
+
+from ..model import KV
+from ..model import Row
+from ..model import Rows
+
+
+def parse_date(raw: str) -> date | None:
+    if not raw.strip():
+        return None
+    return datetime.strptime(raw.strip(), '%d/%m/%y').date()
+
+
+def parse_decimal(raw: str) -> Decimal | None:
+    if not raw.strip():
+        return None
+    return Decimal(raw.rstrip().replace('.', '').replace(',', '.'))
+
+
+def parse_row(row: str) -> Row:
+    return Row(parse_date(row[0:12]) or date.min,
+               parse_date(row[12:24]) or date.min,
+               parse_decimal(row[24:36]),
+               parse_decimal(row[36:48]),
+               row[48:-2])
+
+
+@overload
+def read_scansioni(fn: str) -> tuple[KV, list[Row]]: ...
+
+
+@overload
+def read_scansioni(fn: str, name: str) -> tuple[KV, Rows]: ...
+
+
+def read_scansioni(fn: str, name: str | None = None) -> tuple[KV, list[Row] | Rows]:
+    with open(fn) as f:
+        all_rows = [parse_row(row) for row in f.readlines()]
+
+    iniziale, *rows, finale = all_rows
+    rows.reverse()
+
+    kv = KV(iniziale.data_contabile, finale.data_contabile, '', '',
+            '', finale.data_contabile, iniziale.money, finale.money)
+    return kv, (rows if name is None else Rows(name, rows))
--- movs/scansioni/__main__.py
+++ movs/scansioni/__main__.py
@@ -1,16 +1,28 @@
+#!/usr/bin/env python
+
 from sys import argv
 
-from tabula.io import read_pdf
+from . import read_scansioni
+
+
+def check_scansioni(fn: str) -> None:
+    print(f'{fn=}')
+
+    kv, rows = read_scansioni(fn)
+
+    iniziale_money = kv.saldo_contabile
+    finale_money = kv.saldo_disponibile
+
+    print(f'{iniziale_money=}')
+    print(f'{finale_money=}')
+    actual = sum((row.money for row in rows), iniziale_money)
+    print(f'      {actual=}')
+    assert finale_money == actual
 
 
 def main() -> None:
     for fn in argv[1:]:
-        print(f'{fn=}')
-        tables = read_pdf(fn, pages=1, pandas_options={'header': None})
-        print(f'{len(tables)=}')
-        for table in tables:
-            print('-----------------------')
-            print(table)
+        check_scansioni(fn)
 
 
 if __name__ == '__main__':
