1585f51fe663fa75f1d16956eb8d9f62aa0a0a9e
Vito De Tullio <vito.detullio@gmail.com>
2023-09-17 17:21:57 +0200

    additional check

--- movs/scansioni/__main__.py
+++ movs/scansioni/__main__.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 
+from decimal import Decimal
 from sys import argv
 
 from . import read_scansioni
 
 
-def check_scansioni(fn: str) -> None:
+def check_scansioni(fn: str) -> tuple[Decimal, Decimal]:
     print(f'{fn=}')
 
     kv, rows = read_scansioni(fn)
@@ -18,11 +19,16 @@ def check_scansioni(fn: str) -> None:
     actual = sum((row.money for row in rows), iniziale_money)
     print(f'      {actual=}')
     assert finale_money == actual, f'{finale_money-actual=}'
+    return iniziale_money, finale_money
 
 
 def main() -> None:
+    previous_finale: Decimal | None = None
     for fn in argv[1:]:
-        check_scansioni(fn)
+        iniziale, finale = check_scansioni(fn)
+        if previous_finale is not None:
+            assert previous_finale == iniziale, f'{previous_finale=}, {iniziale=}'
+        previous_finale = finale
 
 
 if __name__ == '__main__':
