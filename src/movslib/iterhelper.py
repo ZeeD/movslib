1c6ef77f11228e121fee2fee6b478a820dcb619a
Vito De Tullio <vito.detullio@gmail.com>
2025-02-19 16:25:23 +0100

    Update iterhelper.py

--- src/movslib/iterhelper.py
+++ src/movslib/iterhelper.py
@@ -6,11 +6,11 @@ from typing import overload
 
 @overload
 def zip_with_next[T](
-    it: 'Iterable[T]', last: None
-) -> 'Iterable[tuple[T, T | None]]': ...
+    it: Iterable[T], last: None
+) -> Iterable[tuple[T, T | None]]: ...
 @overload
-def zip_with_next[T](it: 'Iterable[T]', last: T) -> 'Iterable[tuple[T, T]]': ...
+def zip_with_next[T](it: Iterable[T], last: T) -> Iterable[tuple[T, T]]: ...
 def zip_with_next[T](
-    it: 'Iterable[T]', last: T | None
-) -> 'Iterable[tuple[T, T | None]]':
+    it: Iterable[T], last: T | None
+) -> Iterable[tuple[T, T | None]]:
     return cast(Iterable[tuple[T, T | None]], pairwise((*it, last)))
